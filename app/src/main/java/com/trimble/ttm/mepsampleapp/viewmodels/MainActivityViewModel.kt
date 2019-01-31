74c43001158ea5b5136977dfcb3856ca855b6e5e
Michael Bayles <mbayles@peoplenetonline.com>
2019-01-31 11:27:48 -0600

    Fixed unit test

--- app/src/main/java/com/trimble/ttm/mepsampleapp/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/trimble/ttm/mepsampleapp/viewmodels/MainActivityViewModel.kt
@@ -5,15 +5,13 @@ import com.trimble.ttm.backbone.api.Backbone
 import com.trimble.ttm.backbone.api.BackboneKeys
 import com.trimble.ttm.backbone.api.GpsData
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
 
     fun getGpsLocation(backbone: Backbone): GpsData {
-        val result = backbone.fetch(listOf(BackboneKeys.GPS_DEGREES_KEY))
-        // If we list is empty we have not GPS data. If you want to wait for data
+        val result = backbone.fetch(BackboneKeys.GPS_DEGREES_KEY) ?: throw Exception("No GPS data available")
+        // If the result is null, then we have no GPS data. If you want to wait for data
         // use the asynchronous call.
-        if(result.values.isEmpty())
-            throw Exception("No GPS data available")
-        val x = result.values.first()
-        return x.valueAs()
+
+        return result.valueAs()
     }
 }
