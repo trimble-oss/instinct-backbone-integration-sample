f4097ac565bafa5eff7ef07caa82de071fbb81e0
Brett McKay <brett_mckay@trimble.com>
2019-02-21 13:36:29 -0700

    Add gear and ignition views
    MAINE-574

--- app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
+++ app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+enum class IGNITION_STATE {
+    OFF,
+    ACCESSORY,
+    ENGINE_ON,
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         speedometer.speedTo(50f)
+
+        gear.currentProgress = 3
+        setIgnintionTo(IGNITION_STATE.ACCESSORY)
+    }
+
+    private fun setIgnintionTo(state: IGNITION_STATE) {
+        ignition_off.setImageResource(R.drawable.off_circle)
+        accessory.setImageResource(R.drawable.off_circle)
+        engine_on.setImageResource(R.drawable.off_circle)
+        when (state) {
+            IGNITION_STATE.OFF -> ignition_off.setImageResource(R.drawable.ignition_off_circle)
+            IGNITION_STATE.ACCESSORY -> accessory.setImageResource(R.drawable.ignition_accessory_circle)
+            IGNITION_STATE.ENGINE_ON -> engine_on.setImageResource(R.drawable.engine_on_circle)
+        }
     }
 }
