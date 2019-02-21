bfd6c9ab7ecbbf067f4b883b5aebea17164b46cb
Brett McKay <brett_mckay@trimble.com>
2019-02-21 16:20:27 -0700

    Add latency graph
    MAINE-574

--- app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
+++ app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.trimble.ttm.mepsampleapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.anychart.AnyChart.box
+import com.anychart.chart.common.dataentry.BoxDataEntry
 import kotlinx.android.synthetic.main.activity_main.*
 
 enum class IGNITION_STATE {
@@ -19,6 +21,17 @@ class MainActivity : AppCompatActivity() {
 
         gear.currentProgress = 3
         setIgnintionTo(IGNITION_STATE.ACCESSORY)
+
+        latency_chart.setChart(box().apply {
+            box(listOf(BoxDataEntry("Latency (ms)", 2000, 2120, 2300, 2430, 2500))).apply {
+                normal().apply {
+                    fill("#00e6e6")
+                    medianStroke("white", 4, "10 0", "miter", "butt")
+                }
+                selected().fill("#00e6e6")
+            }
+            background().fill("#151616")
+        })
     }
 
     private fun setIgnintionTo(state: IGNITION_STATE) {
