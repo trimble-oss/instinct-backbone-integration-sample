a43cad204deeeb8750a52f4b7994f50bfa63069a
Michael Bayles <mbayles@peoplenetonline.com>
2019-02-26 09:56:03 -0600

    Reverting code changes

--- app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
+++ app/src/main/java/com/trimble/ttm/mepsampleapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.trimble.ttm.backbone.api.BackboneFactory
 import com.trimble.ttm.backbone.api.GpsData
@@ -15,47 +14,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var _latitudeView: TextView
     private lateinit var _longitudeView: TextView
 
-    private lateinit var _latitudeStreamView: TextView
-    private lateinit var _longitudeStreamView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         _latitudeView = findViewById(R.id.latitude_value)
         _longitudeView = findViewById(R.id.longitude_value)
-        _latitudeStreamView = findViewById(R.id.latitude_stream_value)
-        _longitudeStreamView = findViewById(R.id.longitude_stream_value)
 
-        val backbone = BackboneFactory.rxBackbone(applicationContext)
+        val backbone = BackboneFactory.backbone(applicationContext)
         val model = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
 
         findViewById<Button>(R.id.get_gps_data).setOnClickListener {
             val gpsData = model.getGpsLocation(backbone)
             populateGpsData(gpsData)
         }
-        val triggerGpsStreamButton = findViewById<Button>(R.id.get_gps_data_stream)
-        triggerGpsStreamButton.setOnClickListener{
-            if(model.gpsLocationStreamInProgress.value!!)
-                model.stopGpsLocationStream()
-            else
-                model.triggerGpsLocationStream(backbone)
-        }
-
-        model.gpsLocationStream.observe(this, gpsObserver)
-        model.gpsLocationStreamInProgress.observe(this, Observer{
-            if(it) triggerGpsStreamButton.text = getString(R.string.stop_gps_stream)
-            else triggerGpsStreamButton.text = getString(R.string.start_gps_stream)
-        })
     }
 
     private fun populateGpsData(gpsData: GpsData) {
         _latitudeView.text = gpsData.latitudeInDegrees.toString()
         _longitudeView.text = gpsData.longitudeInDegrees.toString()
-    }
-
-    private val gpsObserver = Observer<GpsData> { gpsData ->
-        _latitudeStreamView.text = gpsData.latitudeInDegrees.toString()
-        _longitudeStreamView.text = gpsData.longitudeInDegrees.toString()
     }
 }
--- app/src/test/java/com/trimble/ttm/mepsampleapp/ExampleUnitTest.kt
+++ app/src/test/java/com/trimble/ttm/mepsampleapp/ExampleUnitTest.kt
@@ -1,20 +1,23 @@
 package com.trimble.ttm.mepsampleapp
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.trimble.ttm.backbone.api.Backbone
 import com.trimble.ttm.backbone.api.BackboneKeys
-import com.trimble.ttm.backbone.api.RxBackbone
 import com.trimble.ttm.mepsampleapp.viewmodels.MainActivityViewModel
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.Test
+
 
 class MainActivityViewModelUnitTest {
     @Test(expected = Exception::class)
     fun `getGpsLocation should throw exception if no result`() {
 
-        val backbone: RxBackbone = mockk()
-        every { backbone.fetch(BackboneKeys.GPS_DEGREES_KEY) } returns null
+        val backbone: Backbone = mock()
 
         val mainActivityViewModel = MainActivityViewModel()
+        whenever(backbone.fetch(BackboneKeys.GPS_DEGREES_KEY))
+            .thenReturn(null)
+
         mainActivityViewModel.getGpsLocation(backbone)
     }
 }
