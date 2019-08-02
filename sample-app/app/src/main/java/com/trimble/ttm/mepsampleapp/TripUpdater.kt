4f46b4319e2fbaebdc557eb1290fb408223c5ab0
Brett McKay <brett_mckay@trimble.com>
2019-08-02 15:46:11 -0600

    Update app to use Backbone 2
    MAINE-985

--- sample-app/app/src/main/java/com/trimble/ttm/mepsampleapp/TripUpdater.kt
+++ sample-app/app/src/main/java/com/trimble/ttm/mepsampleapp/TripUpdater.kt
@@ -4,24 +4,17 @@
 
 package com.trimble.ttm.mepsampleapp
 
-import com.trimble.ttm.backbone.api.BackboneKeys.ENGINE_ODOMETER_KM_KEY
-import com.trimble.ttm.backbone.api.BackboneKeys.TIME_ENGINE_ON_SECONDS_KEY
-import com.trimble.ttm.backbone.api.BackboneResult
 import com.trimble.ttm.mepsampleapp.view.Trip
 
 class TripUpdater {
     private var distanceAtStart: Int = 0
     private var prevTimeOn: Long = Long.MAX_VALUE
 
-    fun with(result: BackboneResult): Trip? {
-        val odometer = result[ENGINE_ODOMETER_KM_KEY]?.valueAs<Int>() ?: 0
+    fun with(odometer: Int, timeEngineOn: Long): Trip {
+        if (timeEngineOn < prevTimeOn) distanceAtStart = odometer
 
-        return result[TIME_ENGINE_ON_SECONDS_KEY]?.valueAs<Long>()?.let { timeOn ->
-            if (timeOn < prevTimeOn) distanceAtStart = odometer
+        prevTimeOn = timeEngineOn
 
-            prevTimeOn = timeOn
-
-            Trip(timeOn, odometer - distanceAtStart)
-        }
+        return Trip(timeEngineOn, odometer - distanceAtStart)
     }
 }
