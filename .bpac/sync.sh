c88b0969203853dc933f73a1e7021b59816907f2
Hawkmoon Instinct <aws.preprod@peoplenetonline.com>
2024-02-01 16:25:00 +0000

    Sync 2024-02-01-16:25:00

--- .bpac/sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-
-[ -d "$$HOME/.ssh" ] || mkdir -p "$$HOME/.ssh"
-
-cp ./.bpac/ssh_config "$HOME/.ssh/config"
-
-apk add rsync && apk add bash && bash ./.bpac/gitsync.sh
